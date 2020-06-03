0e089458059816eb3d3abc9b25df8824c685ead9
A-D-D <toni2030@gmail.com>
2020-06-03 15:56:27 +0300

    web file

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'HomeController@hello');
-/*
-Route::get('/hi/{username}', function ($username) {
-    return view('hi ').$username;
-});*/
+
 
 Route::get('/FirstPage', function () {
     return view('first_page');
